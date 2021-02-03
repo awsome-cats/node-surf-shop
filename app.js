1f676191953d55f8ca1a211b256b8ab9c135aa0c
awsome-cats <cattwin82@gmail.com>
2021-02-03 22:57:13 +0900

    four commit add routes on index.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const index = require('./routes/index');
 const posts = require('./routes/posts');
 const reviews = require('./routes/reviews');
 
-const  users = require('./routes/users');
 
 const  app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,57 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+/* GET /register. */
+router.get('/register', (req, res) => {
+  res.send('GET /register')
+})
+
+/* GET /register. */
+router.post('/register', (req, res,) => {
+  res.send('POST /register');
+});
+
+/* GET /register. */
+router.get('/login', (req, res,) => {
+  res.send('GET /login');
+});
+
+/* GET /register. */
+router.post('/login', (req, res,) => {
+  res.send('POST /login');
+});
+
+
+router.get('/profile', (req, res) => {
+  res.send('GET /profile')
+})
+
+
+
+router.put('/profile/:user_id', (req, res) => {
+  res.send('POST /profile')
+})
+
+
+router.get('/forgot-pw', (req, res) => {
+  res.send('GET /forgot-password')
+})
+
+router.put('/forgot-pw', (req, res) => {
+  res.send('PUT /forgot-password')
+})
+
+router.get('/reset-pw/:token', (req, res) => {
+  res.send('GET /rest-pw/:token')
+})
+
+router.put('/reset-pw/:token', (req, res) => {
+  res.send('PUT /rest-pw/:token')
+})
+// router.delete('/profile/:user_id', (req, res) => {
+//   res.send('POST /profile')
+// })
+
+
+
 module.exports = router;
